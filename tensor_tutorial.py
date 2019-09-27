5348cb55379134e1334ba8d76e88d6b36291097a
henry <henry.zhang90@gmail.com>
2019-09-27 12:26:52 -0700

    set up file

---
+++ tensor_tutorial.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 11:50:36 2019
+
+@author: henry
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
