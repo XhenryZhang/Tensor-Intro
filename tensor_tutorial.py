9d2cca77ee66cfa3f1ec236f4ab242aa3d4094dd
henry <henry.zhang90@gmail.com>
2019-09-29 17:51:50 -0700

    added prediction visualization

--- tensor_tutorial.py
+++ tensor_tutorial.py
@@ -15,6 +15,39 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
+# plots the image
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.imshow(img, cmap=plt.cm.binary) # shows image
+    
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+        
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+               100*np.max(predictions_array),
+               class_names[true_label]), color = color)
+
+# makes bar plot
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(True)
+    plt.xticks(range(10))
+    plt.yticks([])
+    # makes bar plot
+    thisplot = plt.bar(range(10), predictions_array, color="#777777") # bar graph
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red') # sets color red if incorrect
+    thisplot[true_label].set_color('blue')
+
 # import fashion MNIST data
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -32,7 +65,7 @@ for i in range(25):
     plt.yticks([])
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
+    plt.xlabel(class_names[train_labels[i]]) # labels X-axis
 plt.show()
 
 # make model with 3 layers
@@ -57,3 +90,34 @@ print('\nTest Accuracy:', test_acc)
 
 # see the predictions
 predictions = model.predict(test_images)
+# list of the output layers of all test images
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*3*num_cols, 3*num_rows)) # changes up the figure size
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1) # get the plot ready to plot
+  # location of plot, given index and dimensions of the plt
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout() # everything clumped together
+plt.show()
+
+img = test_images[1]
+# keras models make predictions on a batch at once
+# so, we need to add the image to the list
+img = (np.expand_dims(img, 0))
+
+predictions_single = model.predict(img)
